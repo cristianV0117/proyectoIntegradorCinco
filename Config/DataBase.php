88cd5af0153711fd90b4bf21f128ade98d844483
cristianV0117 <cvo6372@gmail.com>
2021-04-29 16:07:31 -0500

    database
    database

--- Config/DataBase.php
+++ Config/DataBase.php
@@ -4,10 +4,10 @@ abstract class DataBase
     protected function dataBase()
     {
         return [
-            "server" => "127.0.0.1",
-            "dataBase" => "test",
-            "user" => "root",
-            "pass" => ""
+            "server" => "us-cdbr-east-03.cleardb.com",
+            "dataBase" => "heroku_53981faae2b1da7",
+            "user" => "bf058b6433589c",
+            "pass" => "ee09700c"
         ];
     }
 }
